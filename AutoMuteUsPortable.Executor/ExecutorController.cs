6fc9c1b4d7576bbfc62fddfb47844eea473300a3
mtaku3 <m.taku3.1222@gmail.com>
2023-01-15 12:47:00 +0900

    fix: fix wrong implementation which hides base class member

--- AutoMuteUsPortable.Executor/ExecutorController.cs
+++ AutoMuteUsPortable.Executor/ExecutorController.cs
@@ -16,7 +16,6 @@ namespace AutoMuteUsPortable.Executor;
 public class ExecutorController : ExecutorControllerBase
 {
     private readonly PocketBaseClientApplication _pocketBaseClientApplication = new();
-    public new readonly ExecutorConfiguration ExecutorConfiguration;
     private Process? _process;
 
     public ExecutorController(object executorConfiguration) : base(executorConfiguration)
