8a2bc2767c57df1f0cba8cca96c0016a3d6359c4
mtaku3 <m.taku3.1222@gmail.com>
2023-02-10 20:22:16 +0900

    fix: typo

--- AutoMuteUsPortable.Executor/ExecutorController.cs
+++ AutoMuteUsPortable.Executor/ExecutorController.cs
@@ -16,14 +16,14 @@ public class ExecutorController : ExecutorControllerBase
     private readonly PocketBaseClientApplication _pocketBaseClientApplication = new();
     private Process? _process;
     private readonly StreamWriter _outputStreamWriter;
-    private readonly StreamWriter _errorstreamWriter;
+    private readonly StreamWriter _errorStreamWriter;
 
     public ExecutorController(object executorConfiguration) : base(executorConfiguration)
     {
         #region Initialize stream writer
 
         _outputStreamWriter = new StreamWriter(OutputStream);
-        _errorstreamWriter = new StreamWriter(ErrorStream);
+        _errorStreamWriter = new StreamWriter(ErrorStream);
 
         #endregion
 
@@ -74,7 +74,7 @@ public class ExecutorController : ExecutorControllerBase
         #region Initialize stream writer
 
         _outputStreamWriter = new StreamWriter(OutputStream);
-        _errorstreamWriter = new StreamWriter(ErrorStream);
+        _errorStreamWriter = new StreamWriter(ErrorStream);
 
         #endregion
 
@@ -434,6 +434,6 @@ public class ExecutorController : ExecutorControllerBase
 
     private void ProcessOnErrorDataReceived(object sender, DataReceivedEventArgs e)
     {
-        _errorstreamWriter.Write(e.Data);
+        _errorStreamWriter.Write(e.Data);
     }
 }
